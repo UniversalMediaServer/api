7235f0bf17b14c4490eaafebc9560cc3cb34a0ab
Kyle Martin <kyle.martin@hotmail.co.nz>
2019-10-20 15:57:29 +1300

    code cleanup

--- src/controllers/media.ts
+++ src/controllers/media.ts
@@ -5,13 +5,13 @@ export const getByOsdbHash = (req: Request, res: Response, next: NextFunction) =
   MediaMetadata.findOne({osdbHash: req.params.osdbhash}, (err: any, meta: MediaMetadataInterface) => {
     if (err) {
       return next(err);
-    } else {
-      if (meta) {
-        return res.json(meta);
-      } 
-
-      return res.sendStatus(204);
-      
     }
+
+    if (meta) {
+      return res.json(meta);
+    } 
+
+    return res.sendStatus(204);
+
   });
 };
