904663b448a8df8451237b7b3d402ddfd408884b
Kyle Martin <kyle.martin@hotmail.co.nz>
2019-10-21 21:34:59 +1300

    arrows

--- src/app.ts
+++ src/app.ts
@@ -32,12 +32,12 @@ app.use('/users', usersRouter);
 app.use('/api/media', mediaRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req: Request, res: Response, next: NextFunction) {
+app.use((req: Request, res: Response, next: NextFunction) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req: Request, res: Response, next: NextFunction) {
+app.use((err, req: Request, res: Response, next: NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
