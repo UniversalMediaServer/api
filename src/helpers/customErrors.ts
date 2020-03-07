7d478d9609d8d3e080348c616bbe5b84a0ee2cac
Klaus Townsend <subjunk@gmail.com>
2020-03-07 21:00:46 +1300

    Formatting

--- src/helpers/customErrors.ts
+++ src/helpers/customErrors.ts
@@ -1,10 +1,7 @@
-export const ValidationError = function ValidationError(message: string): void {
-  const error = Error.call(this, message);
-
-  this.name = 'ValidationError';
-  this.message = error.message;
-  this.stack = error.stack;
-};
-
-ValidationError.prototype = Object.create(Error.prototype);
-ValidationError.prototype.constructor = ValidationError;
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
