e0115977e232fba42f8fba648735047ce34da9df
Kyle Martin <kyle.martin@hotmail.co.nz>
2020-04-13 15:54:27 +1200

    mock opensubtitles in e2e tests (#86)
    * mock opensubtitles in e2e tests
    
    * fixup
    
    * remove yarn build
    
    we use ts-node instead

--- test/e2e/media.spec.ts
+++ test/e2e/media.spec.ts
@@ -22,7 +22,6 @@ const theSimpsonsMetaData = {
   title: 'The Simpsons Movie',
 };
 const appUrl = 'http://localhost:3000';
-const testOpenSubtitles =  process.env.OS_API_USERAGENT && (process.env.OS_API_USERAGENT !== 'TemporaryUserAgent');
 
 describe('Media Metadata endpoints', () => {
   beforeAll(async() => {
@@ -31,6 +30,7 @@ describe('Media Metadata endpoints', () => {
     await mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
     await MediaMetadataModel.create(interstellarMetaData);
     require('../mocks');
+    require('../opensubtitles-mocks');
     require('../../src/app');
   });
 
@@ -47,59 +47,58 @@ describe('Media Metadata endpoints', () => {
       expect(res.body).toHaveProperty('osdbHash', interstellarMetaData.osdbHash);
       expect(res.body).toHaveProperty('title', interstellarMetaData.title);
     });
-    if (testOpenSubtitles) {
-      it('should return a valid response for a new osdbhash, then store it', async() => {
-        // using example file from https://trac.opensubtitles.org/projects/opensubtitles/wiki/HashSourceCodes
-        const res: any = await got(`${appUrl}/api/media/${theSimpsonsMetaData.osdbHash}/12909756`, { responseType: 'json' });
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toHaveProperty('_id');
-        expect(res.body).toHaveProperty('year', '2007');
-        expect(res.body).toHaveProperty('osdbHash', theSimpsonsMetaData.osdbHash);
-        expect(res.body).toHaveProperty('title', 'The Simpsons Movie');
-        expect(res.body).toHaveProperty('imdbID', 'tt0462538');
-        expect(res.body).toHaveProperty('type', 'movie');
-        expect(res.body).toHaveProperty('goofs');
-        expect(res.body).toHaveProperty('trivia');
-        expect(res.body).toHaveProperty('tagline');
-        // from IMDb API
-        expect(res.body).toHaveProperty('ratings', [
-          { Source: 'Internet Movie Database', Value: '7.3/10' },
-          { Source: 'Rotten Tomatoes', Value: '88%' },
-          { Source: 'Metacritic', Value: '80/100' },
-        ]);
-        expect(res.body).toHaveProperty('metascore', '80');
-        expect(res.body).toHaveProperty('poster', 'https://m.media-amazon.com/images/M/MV5BMTgxMDczMTA5N15BMl5BanBnXkFtZTcwMzk1MzMzMw@@._V1_SX300.jpg');
-        expect(res.body).toHaveProperty('rated', 'PG-13');
-        expect(res.body).toHaveProperty('rating', 7.3);
-        expect(res.body).toHaveProperty('runtime', '87 min');
-        expect(res.body).toHaveProperty('votes', '298,859');
-        expect(res.body).toHaveProperty('boxoffice', '$183,100,000');
-        expect(res.body).toHaveProperty('genres', ['Animation', 'Adventure', 'Comedy']);
-        expect(res.body).toHaveProperty('actors', ['Dan Castellaneta', 'Julie Kavner', 'Nancy Cartwright', 'Yeardley Smith', 'Hank Azaria', 'Harry Shearer', 'Pamela Hayden', 'Tress MacNeille', 'Albert Brooks', 'Karl Wiedergott', 'Marcia Wallace', 'Russi Taylor', 'Maggie Roswell', 'Phil Rosenthal', 'Billie Joe Armstrong']);
+
+    it('should return a valid response for a new osdbhash, then store it', async() => {
+      // using example file from https://trac.opensubtitles.org/projects/opensubtitles/wiki/HashSourceCodes
+      const res: any = await got(`${appUrl}/api/media/${theSimpsonsMetaData.osdbHash}/12909756`, { responseType: 'json' });
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toHaveProperty('_id');
+      expect(res.body).toHaveProperty('year', '2007');
+      expect(res.body).toHaveProperty('osdbHash', theSimpsonsMetaData.osdbHash);
+      expect(res.body).toHaveProperty('title', 'The Simpsons Movie');
+      expect(res.body).toHaveProperty('imdbID', 'tt0462538');
+      expect(res.body).toHaveProperty('type', 'movie');
+      expect(res.body).toHaveProperty('goofs');
+      expect(res.body).toHaveProperty('trivia');
+      expect(res.body).toHaveProperty('tagline');
+      // from IMDb API
+      expect(res.body).toHaveProperty('ratings', [
+        { Source: 'Internet Movie Database', Value: '7.3/10' },
+        { Source: 'Rotten Tomatoes', Value: '88%' },
+        { Source: 'Metacritic', Value: '80/100' },
+      ]);
+      expect(res.body).toHaveProperty('metascore', '80');
+      expect(res.body).toHaveProperty('poster', 'https://m.media-amazon.com/images/M/MV5BMTgxMDczMTA5N15BMl5BanBnXkFtZTcwMzk1MzMzMw@@._V1_SX300.jpg');
+      expect(res.body).toHaveProperty('rated', 'PG-13');
+      expect(res.body).toHaveProperty('rating', 7.3);
+      expect(res.body).toHaveProperty('runtime', '87 min');
+      expect(res.body).toHaveProperty('votes', '298,859');
+      expect(res.body).toHaveProperty('boxoffice', '$183,100,000');
+      expect(res.body).toHaveProperty('genres', ['Animation', 'Adventure', 'Comedy']);
+      expect(res.body).toHaveProperty('actors', ['Dan Castellaneta', 'Julie Kavner', 'Nancy Cartwright', 'Yeardley Smith', 'Hank Azaria', 'Harry Shearer', 'Pamela Hayden', 'Tress MacNeille', 'Albert Brooks', 'Karl Wiedergott', 'Marcia Wallace', 'Russi Taylor', 'Maggie Roswell', 'Phil Rosenthal', 'Billie Joe Armstrong']);
+  
+      // should save to db
+      const doc = await MediaMetadataModel.findOne({ osdbHash: res.body.osdbHash });
+  
+      expect(doc).toHaveProperty('_id');
+      expect(doc).toHaveProperty('year', '2007');
+      expect(doc).toHaveProperty('osdbHash', theSimpsonsMetaData.osdbHash);
+      expect(doc).toHaveProperty('title', 'The Simpsons Movie');
+      expect(doc).toHaveProperty('imdbID', 'tt0462538');
+      expect(doc).toHaveProperty('type', 'movie');
+      expect(doc).toHaveProperty('goofs');
+      expect(doc).toHaveProperty('trivia');
+      expect(doc).toHaveProperty('tagline');
+    });
     
-        // should save to db
-        const doc = await MediaMetadataModel.findOne({ osdbHash: res.body.osdbHash });
-    
-        expect(doc).toHaveProperty('_id');
-        expect(doc).toHaveProperty('year', '2007');
-        expect(doc).toHaveProperty('osdbHash', theSimpsonsMetaData.osdbHash);
-        expect(doc).toHaveProperty('title', 'The Simpsons Movie');
-        expect(doc).toHaveProperty('imdbID', 'tt0462538');
-        expect(doc).toHaveProperty('type', 'movie');
-        expect(doc).toHaveProperty('goofs');
-        expect(doc).toHaveProperty('trivia');
-        expect(doc).toHaveProperty('tagline');
-      });
-    
-      it('should create a failed lookup document when Open Subtitles cannot find metadata', async() => {
-        await FailedLookupsModel.deleteMany({});
-        const response: any = await got(`${appUrl}/api/media/f4245d9379d31e30/1234`);
-        expect(response.body).toBe('Metadata not found on OpenSubtitles');
-        const doc = await FailedLookupsModel.findOne({ osdbHash: 'f4245d9379d31e30' });
-        expect(doc).toHaveProperty('_id');
-        expect(doc).toHaveProperty('osdbHash');
-      });
-    }
+    it('should create a failed lookup document when Open Subtitles cannot find metadata', async() => {
+      await FailedLookupsModel.deleteMany({});
+      const response: any = await got(`${appUrl}/api/media/f4245d9379d31e30/1234`);
+      expect(response.body).toBe('Metadata not found on OpenSubtitles');
+      const doc = await FailedLookupsModel.findOne({ osdbHash: 'f4245d9379d31e30' });
+      expect(doc).toHaveProperty('_id');
+      expect(doc).toHaveProperty('osdbHash');
+    });
   });
 
   describe('get by title', () => {
