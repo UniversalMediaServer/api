ecd24dde889215a55ed879f07852ea1b3b06b91d
SubJunk <SubJunk@users.noreply.github.com>
2021-09-14 18:06:53 +1200

    Update src/controllers/info.ts
    Co-authored-by: Kyle Martin <kyle.martin@hotmail.co.nz>

--- src/controllers/info.ts
+++ src/controllers/info.ts
@@ -4,7 +4,7 @@ import TMDBConfiguration from '../models/TMDBConfiguration';
 import { moviedb } from './media';
 
 export const getTMDBImageBaseURL = async(ctx: ParameterizedContext): Promise<{ imageBaseURL: string }> => {
-  const configuration = await TMDBConfiguration.findOne().lean();
+  const configuration = await TMDBConfiguration.findOne().lean().exec();
   if (configuration) {
     return { imageBaseURL: configuration.imageBaseURL };
   }
