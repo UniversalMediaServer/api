cb2d1ac04ccb80601b529ec83fe052e3654e6bce
Klaus Townsend <subjunk@gmail.com>
2021-10-23 16:43:26 +1300

    Added season and configuration subversions, and simplified

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -1,41 +1,37 @@
 import * as Router from 'koa-router';
 import * as MediaController from '../controllers/media';
-import { mediaApiSubversions } from '../helpers/subversioning';
+import { subversions } from '../helpers/subversioning';
 
 const router = new Router({ prefix: '/api/media' });
 
 router.get('/osdbhash/:osdbhash/:filebytesize', async(ctx) => {
-  ctx.set('X-Api-Subversion', mediaApiSubversions['/osdbhash/:osdbhash/:filebytesize']);
   await MediaController.getByOsdbHash(ctx);
 });
 
 router.get('/title', async(ctx) => {
-  ctx.set('X-Api-Subversion', mediaApiSubversions['/title']);
   await MediaController.getBySanitizedTitle(ctx);
 });
 
 router.get('/v2/title', async(ctx) => {
-  ctx.set('X-Api-Subversion', mediaApiSubversions['/v2/title']);
   await MediaController.getBySanitizedTitleV2(ctx);
 });
 
 router.get('/imdbid', async(ctx) => {
-  ctx.set('X-Api-Subversion', mediaApiSubversions['/imdbid']);
   await MediaController.getByImdbID(ctx);
 });
 
 router.get('/seriestitle', async(ctx) => {
-  ctx.set('X-Api-Subversion', mediaApiSubversions['/seriestitle']);
+  ctx.set('X-Api-Subversion', subversions['series']);
   await MediaController.getSeries(ctx);
 });
 
 router.get('/video', async(ctx) => {
-  ctx.set('X-Api-Subversion', mediaApiSubversions['/video']);
+  ctx.set('X-Api-Subversion', subversions['video']);
   await MediaController.getVideo(ctx);
 });
 
 router.get('/season', async(ctx) => {
-  ctx.set('X-Api-Subversion', mediaApiSubversions['/season']);
+  ctx.set('X-Api-Subversion', subversions['season']);
   await MediaController.getSeason(ctx);
 });
 
