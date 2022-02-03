3255007fa090383c327a395039fd98201efcd60c
Kyle Martin <kyle.martin@hotmail.co.nz>
2022-02-04 10:57:50 +1300

    route metrics (#812)

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -1,20 +1,28 @@
 import * as Router from 'koa-router';
+import * as client from 'prom-client';
 import * as MediaController from '../controllers/media';
 import { subversions } from '../helpers/subversioning';
+
+const seriesTitleCounter = new client.Counter({ name: 'seriestitle_endpoint', help: 'Counter of get requests to /seriestitle' });
+const videoCounter = new client.Counter({ name: 'video_endpoint', help: 'Counter of get requests to /video' });
+const seasonCounter = new client.Counter({ name: 'season_endpoint', help: 'Counter of get requests to /season' });
 
 const router = new Router({ prefix: '/api/media' });
 
 router.get('/seriestitle', async(ctx) => {
+  seriesTitleCounter.inc();
   ctx.set('X-Api-Subversion', subversions['series']);
   await MediaController.getSeries(ctx);
 });
 
 router.get('/video', async(ctx) => {
+  videoCounter.inc();
   ctx.set('X-Api-Subversion', subversions['video']);
   await MediaController.getVideo(ctx);
 });
 
 router.get('/season', async(ctx) => {
+  seasonCounter.inc();
   ctx.set('X-Api-Subversion', subversions['season']);
   await MediaController.getSeason(ctx);
 });
