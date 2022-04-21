2af73f322e91b99312846d56928d2d8ebc5e54e7
Klaus Townsend <subjunk@gmail.com>
2022-04-22 11:10:13 +1200

    Moved helper

--- src/services/deprecated/external-api-helper.ts
+++ src/services/deprecated/external-api-helper.ts
@@ -3,10 +3,12 @@ import * as _ from 'lodash';
 import * as episodeParser from 'episode-parser';
 import * as natural from 'natural';
 
+import { getTMDBImageBaseURL } from '../../controllers/info';
 import { IMDbIDNotFoundError } from '../../helpers/customErrors';
 import { MediaMetadataInterface } from '../../models/MediaMetadata';
 import omdbAPI from '../omdb-api';
 import { mapper } from '../../utils/data-mapper';
+import { SeriesMetadataInterface } from '../../models/SeriesMetadata';
 
 /**
  * @deprecated see getFromOMDbAPIV2
@@ -81,6 +83,55 @@ export const getFromOMDbAPI = async(imdbId?: string, searchRequest?: SearchReque
     metadata = mapper.parseOMDbAPIEpisodeResponse(imdbData);
   } else {
     throw new Error('Received a type we did not expect');
+  }
+
+  return metadata;
+};
+
+/*
+ * If the incoming metadata contains a poster image within the images
+ * array, we populate the poster value with that, and return the whole object.
+ *
+ * This must be done on-the-fly like this because the imageBaseURL can change.
+ */
+export const addPosterFromImages = async(metadata: any): Promise<SeriesMetadataInterface | MediaMetadataInterface> => {
+  if (!metadata) {
+    throw new Error('Metadata is required');
+  }
+
+  if (metadata.poster) {
+    // There is already a poster
+    return metadata;
+  }
+
+  let posterRelativePath: string;
+
+  if (metadata.posterRelativePath) {
+    posterRelativePath = metadata.posterRelativePath;
+  } else {
+    const potentialPosters = metadata?.images?.posters ? metadata?.images?.posters : [];
+    const potentialStills = metadata?.images?.stills || [];
+    const potentialImagesCombined = _.concat(potentialPosters, potentialStills);
+    if (_.isEmpty(potentialImagesCombined)) {
+      // There are no potential images
+      return metadata;
+    }
+
+    const englishImages = _.filter(potentialImagesCombined, { 'iso_639_1': 'en' }) || [];
+    const noLanguageImages = _.filter(potentialImagesCombined, { 'iso_639_1': null }) || [];
+    const posterCandidates = _.merge(noLanguageImages, englishImages);
+    if (!posterCandidates || _.isEmpty(posterCandidates)) {
+      // There are no English or non-language images
+      return metadata;
+    }
+
+    const firstPoster = _.first(posterCandidates);
+    posterRelativePath = firstPoster.file_path;
+  }
+
+  if (posterRelativePath) {
+    const imageBaseURL = await getTMDBImageBaseURL();
+    metadata.poster = imageBaseURL + 'w500' + posterRelativePath;
   }
 
   return metadata;
