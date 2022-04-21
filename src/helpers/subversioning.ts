a2d6df5e09dc4892a38c3afdd89af406ddf988c9
SubJunk <SubJunk@users.noreply.github.com>
2022-04-21 20:25:17 +1200

    Bump video subversion and expand test (#887)
    * Bump video subversion and expand test
    
    * Formatting

--- src/helpers/subversioning.ts
+++ src/helpers/subversioning.ts
@@ -2,11 +2,11 @@
  * These versions get bumped to tell the client that
  * it should update its relevant data.
  * e.g. if we did an API change that improved our results
- * for TV series in any way, we should bump that version by 1.
+ * for videos in any way, we should bump that version by 1.
  */
 export const subversions = {
   'configuration': '1',
   'season': '1',
   'series': '1',
-  'video': '2',
+  'video': '3',
 };
