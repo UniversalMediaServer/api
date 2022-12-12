a4c3d8d3e80e3f8ed7fb636de01197417a12a92b
SurfaceS <nicoooo0@outlook.com>
2022-12-12 17:48:53 +0100

    updated omdb replies

--- test/mocks.js
+++ test/mocks.js
@@ -222,6 +222,12 @@ nock('https://www.omdbapi.com:443', { 'encodedQueryParams': true })
 nock('https://www.omdbapi.com:443', { 'encodedQueryParams': true })
   .persist()
   .get('/')
+  .query({"apikey":"foo","r":"json","i":"tt0411008","Season":"1"})
+  .reply(200, {"Response":"False","Error":"Series or season not found!"});
+
+nock('https://www.omdbapi.com:443', { 'encodedQueryParams': true })
+  .persist()
+  .get('/')
   .query({"apikey":"foo","r":"json","i":"tt0411008","Season":"2"})
   .reply(200, {"Title":"Lost","Season":"2","totalSeasons":"3","Episodes":[{"Title":"Live Together, Die Alone: Part 1","Released":"2006-05-24","Episode":"23","imdbRating":"9.4","imdbID":"tt0793130"}],"Response":"True"});
 
@@ -229,6 +235,12 @@ nock('https://www.omdbapi.com:443', { 'encodedQueryParams': true })
   .persist()
   .get('/')
   .query({"apikey":"foo","r":"json","i":"tt0417299","Season":"1"})
+  .reply(200, {"Response":"False","Error":"Series or season not found!"});
+
+nock('https://www.omdbapi.com:443', { 'encodedQueryParams': true })
+  .persist()
+  .get('/')
+  .query({"apikey":"foo","r":"json","i":"tt0417299","Season":"2"})
   .reply(200, {"Response":"False","Error":"Series or season not found!"});
 
 // https://www.omdbapi.com/?Season=1&apikey=foo&i=tt0411008&r=json
