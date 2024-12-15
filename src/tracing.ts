6c1e4cf0e8b82192373f4f96a75e4263e5b2afce
Kyle <kyle.martin@hotmail.co.nz>
2024-12-16 09:44:32 +1300

    feat: OpenTelemetry instrumentation

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,8 +1,8 @@
-const opentelemetry = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { Resource } = require('@opentelemetry/resources');
-const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { Resource } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 
 const exporterOptions = {
   url: 'http://localhost:4318/v1/traces',
@@ -10,7 +10,7 @@ const exporterOptions = {
 
 const traceExporter = new OTLPTraceExporter(exporterOptions);
 
-const sdk = new opentelemetry.NodeSDK({
+const sdk = new NodeSDK({
   traceExporter,
   instrumentations: [getNodeAutoInstrumentations()],
   resource: new Resource({
