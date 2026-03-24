4dec89eed84e79d6472d174d5ad9c5e7bfbc6589
SubJunk <subjunk@gmail.com>
2026-03-24 17:48:01 +1300

    Use a better way to check index use

--- src/models/FailedLookups.ts
+++ src/models/FailedLookups.ts
@@ -31,7 +31,7 @@ const FailedLookupsSchema = new Schema({
   language: { type: String },
   season: { type: String },
   startYear: { type: String },
-  title: { type: String, required: true },
+  title: { type: String, index: true, required: true },
   tmdbID: { index: true, type: Number },
   year: { type: String },
   failedValidation: { type: Boolean, default: false },
